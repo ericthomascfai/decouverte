f04f928d6b2f543f8853b51a304f9db0f084fa3c
eric <eric.thomas@formation-industries-fc.fr>
2020-09-11 15:58:48 +0200

    MANIPULATION DES VARIABLES EX3

--- main.c
+++ main.c
@@ -32,6 +32,28 @@ int main() {
     printf("les chars:%c,%c\n",c1,c2);
 
     /******************Exercice n°2******************************************************/
+    putchar(84);
+    putchar(97);
+    putchar(116);
+    putchar(97);
+    putchar(70);
+    putchar(97);
+    putchar(105);
+    putchar(116);
+    putchar(68);
+    putchar(117);
+    putchar(90);
+    putchar(101);
+    putchar(108);
+    putchar(101);
+/***************************Exercice n°3***************************************************/
+int saisie=0; //déclarer un variable saisie valoriser à 0 en attente de valorisation par l'utilisateur
+printf("\n1-Commande 1\n"); // \n car on a besoin tout de suite d'un retour à la ligne
+printf("2_Commande 2\n");
+printf("3-Commande 3\n");
+scanf("%d",&saisie);  //formatage entier et valorisation de la variable saisie
+printf("Vous avez choisi la commande %d\n",saisie); //affichage de saisie par formatage %d
+/**************************Exerice n°4******************************************************/
 
     return 0;
 }
